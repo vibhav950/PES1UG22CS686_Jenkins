56c7eb5bdeabe391e93509184662b156728deb22
vibhav950 <vibhavtiwari.official@gmail.com>
2025-03-18 15:32:09 +0530

    Lab8 first edit

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
 //File: hello.cpp
 #include <iostream>
-using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  std::cout << "Hello, World\n";
+  std::cout << "Hello, Jenkins\n";
+  std::cout << "I have successfully built and run this  \n";
+  std::cout << "First edit for Lab #8\n";
+  std::cout << std::endl;
   return 0;
 }
