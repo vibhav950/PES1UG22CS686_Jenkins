77d4b8c64ade71355b67ecaf3f81b42748d2d07d
vibhav950 <vibhavtiwari.official@gmail.com>
2025-03-18 15:46:19 +0530

    Jenkins pipeline trigger

---
+++ main/p1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <random>
+
+int main() {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::cout << "Here is a random number from the mt19937 RNG: ";
+    std::uniform_int_distribution<> dis(1, 50000);
+    std::cout << dis(gen) << std::endl;
+    return 0;
+}
